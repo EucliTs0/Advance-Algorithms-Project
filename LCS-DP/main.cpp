7b877043661e3c1af7a8a60c7c829cf5828c1015
EucliTs0 <dimitri_ts0@hotmail.com>
2016-11-21 16:59:11 +0100

    Update main.cpp

--- LCS-DP/main.cpp
+++ LCS-DP/main.cpp
@@ -5,29 +5,39 @@
 #include <vector>
 #include <fstream>
 #include <stdio.h>
-#include <fstream>
 #include <stdlib.h>
 #include <cctype>
 #include <math.h>
 #include <cmath>
 #include <sstream>
 #include <algorithm>
-
-
+#include <locale.h>
+#include <utf8.h>
+#include <locale>
 
 using namespace std;
 string pNgreedy(string, int);
 void printNeatly(vector<string>, int);
 char *lcs2(char*, char*);
+//string lcs_DnC(string, string);
 char *convert(const std::string & s);
-void lcs(char*, char*);
+int lcs(string, string);
+bool validUTF8_file(const char*);
+void fix_utf8_string(string&);
+//void remove_sw(string, string);
+void stripChars(string&);
 
 int main() {
+    string illegalChars = """""\\/:?,.=''-_+)[]:<>(|";
     int n = 70;
+    int L;
+    //Checking if files are in utf8 encoding
+    validUTF8_file("corpus/g4pD_taskc.txt");
+    validUTF8_file("corpus/orig_taskc.txt");
         ifstream mytextfile;
 
     try {
-        mytextfile.open("g4pC_taska.txt");
+        mytextfile.open("corpus/g4pD_taskc.txt");
         if (!mytextfile) {
             cout << "Error opening file " << endl;
             return 1;
@@ -41,7 +51,7 @@ int main() {
     ifstream originalfile;
 
     try {
-        originalfile.open("orig_taska.txt");
+        originalfile.open("corpus/orig_taskc.txt");
         if (!originalfile) {
             cout << "Error opening file " << endl;
             return 1;
@@ -51,6 +61,27 @@ int main() {
     catch (ifstream::failure()) {
         cout << "Exception opening file " << endl;
     }
+
+    ifstream dictionary;
+    dictionary.open("stop-word-list.txt");
+
+    vector<string> stopwords;
+    string stopwordlist;
+
+    while (dictionary >> stopwordlist) {
+        stopwords.push_back(stopwordlist);
+    }
+
+    stringstream temp;
+    for (int i = 0; i <= stopwords.size() - 1; i++) {
+            if (i != 0)
+            temp << " ";
+            temp << stopwords[i];
+    }
+    //create a string with the stop words
+    string dict = temp.str();
+
+
 
     /* for (int i = 0; i < n; i++)
 	{
@@ -75,9 +106,12 @@ int main() {
 
     }
 
+
     while (originalfile >> word_original) {
         words_original.push_back(word_original);
     }
+
+    //From <vector>string to string
 
     stringstream ss;
     stringstream ss1;
@@ -96,29 +130,72 @@ int main() {
         string text = ss.str();
         string original_text = ss1.str();
 
+        //Remove illegal characters
+        string::iterator it;
+
+        for(it = text.begin(); it < text.end(); it++) {
+            bool found = illegalChars.find(*it) != string::npos;
+            if(found) {
+                *it = ' ';
+            }
+        }
+
+        for(it = original_text.begin(); it < original_text.end(); it++) {
+            bool found = illegalChars.find(*it) != string::npos;
+            if(found) {
+                *it = ' ';
+            }
+        }
+
+        //transform to lower-case
+        transform(text.begin(), text.end(), text.begin(), ::tolower);
+        transform(original_text.begin(), original_text.end(), original_text.begin(), ::tolower);
+
+        //Function to remove stopwords. So slow...
+
+       //removeSubStrings(text, dict);
+       // removeSubStrings(original_text, dict);
+
+
+        //Check if strings are in utf8
+        fix_utf8_string(text);
+        fix_utf8_string(original_text);
+
+        //Remove any non-ASCII characters
+        stripChars(text);
+        stripChars(original_text);
+
+        //From string to char*
         char* wordsc = new char[text.length() + 1];
         char* words_originalc = new char [original_text.length() + 1];
         strcpy(wordsc, text.c_str());
         strcpy(words_originalc, original_text.c_str());
 
+        cout << pNgreedy(text, n);
+        cout << endl;
+        cout << endl;
+        cout << endl;
+
+        cout << pNgreedy(original_text, n);
+        cout << endl;
+        cout << endl;
+        cout << endl;
+
         string result;
         result = lcs2(wordsc, words_originalc);
 
 
-     //cout << text << endl; //
+         cout << endl;
+         cout << "LCS is: " << result;
+         cout << endl;
+         L = lcs(text, original_text);
+         cout << endl;
+         cout << "Length of LCS: " << L;
 
+    // printNeatly(result, n);
     cout << endl;
     cout << endl;
-    cout << "LCS is: " << result;
-    cout << endl;
-    cout << endl;
-    lcs(wordsc, words_originalc);
-
-
-    /* printNeatly(words, n);
-    cout << endl;
-    cout << endl;
-    cout << pNgreedy(text, n) << endl; */
+    cout << pNgreedy(result, n) << endl;
 
     delete [] wordsc;
     delete [] words_originalc;
